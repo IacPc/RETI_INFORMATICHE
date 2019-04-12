442c0a85f0d7e69bf9312515499fd57f937d2395
IacPc <iac.pacini@gmail.com>
2019-04-12 12:05:35 +0200

    implementata interfaccia client relativa a funz. get

--- client.c
+++ client.c
@@ -9,7 +9,9 @@ int main(int argc, char* argv[]){
 	char ip_ser[4];
 	char porta[4];	
 	char file[32];	
-
+	char comando1[6];
+	char comando2[32];
+	char comadno3[32];
 	int tot=0;
 	
     struct sockaddr_in srv_addr;// my_addr;
@@ -30,12 +32,28 @@ int main(int argc, char* argv[]){
     srv_addr.sin_family = AF_INET;
     srv_addr.sin_port = htons(atoi(porta));
     inet_pton(AF_INET, ip_ser, &srv_addr.sin_addr);
-	
-	strcpy(file,"copione.txt\0");
-	strcpy(ag.mode,"txt\0");
-	ag.srv_addr= srv_addr;	
-	ag.sd=sd;
-	strcpy(ag.file,file);
-	tot =get(&ag);
-	printf("scaricati %d byte\n",tot);
+
+
+	printf("sono disponibili i seguenti comandi:\n");
+	printf("!help --> mostra l'elenco dei comandi disponibili\n");
+	printf("!mode{txt|bin} --> impposta il modo di trasferimento dei files (testo o binario)\n");
+	printf("!get filename nome_locale --> richiede al server il nome del <filenamme> e lo salva localment con il nome <nome_locale>\n");
+
+	printf("!quit --> termina il client\n");
+
+	while(1){
+		scanf("%s %s %s",comando1,comando2,comando3);
+		
+		switch(comando1)
+		case("!get"):
+		strcpy(file,"copione.txt\0");
+		strcpy(ag.mode,"txt\0");
+		ag.srv_addr= srv_addr;	
+		ag.sd=sd;
+		strcpy(ag.file,file);
+		tot =get(&ag);
+		printf("scaricati %d byte\n",tot);
+		break
+
+	}
 }
